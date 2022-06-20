41557bd8638b73e63ed793ea089732c0bc9a6ff6
Pepe6bf <pepe6bf@gmail.com>
2022-06-20 17:14:16 +0900

    프로젝트 생성 및 세팅

---
+++ src/test/java/shop/spring/dev/springshop/SpringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.spring.dev.springshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
